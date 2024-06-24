4cd8b2c572974a68d4401212ba3015b5c40b73ef
Kolawole Peter <oluwatayokolawole12@gmail.com>
2024-06-24 12:30:55 +0100

    Project 1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+Subtraction = number1 - number2
+Mulitplication = number1 * number2
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {Subtraction}")
+print(f"Multiplication of {number1} and {number2} is {Mulitplication}")
+
+# Subtraction of 10 and 5 is 5
+# Multiplication of 10 and 5 is 50
+# print(sum)
