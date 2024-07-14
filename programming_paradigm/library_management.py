ea1c5f3690d1bfed8b56b1442ea436c90059cd7a
Kolawole Peter <oluwatayokolawole12@gmail.com>
2024-07-14 19:52:49 +0100

    GG

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,5 +1,25 @@
 # library_management.py
 
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # Initialize as not checked out
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        else:
+            return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        else:
+            return False
+
 class Library:
     def __init__(self):
         self._books = []
